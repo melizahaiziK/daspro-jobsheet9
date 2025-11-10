91d7090344fcc3bdcdd8ad53c55d7234ca3eda4d
melizahaiziK <melizaha1zik@gmail.com>
2025-11-10 15:48:57 +0700

    percobaan 1

---
+++ ArrayBilangan12.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class ArrayBilangan12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int bil [] = {5, 13, -7, 17} ;
+
+        for (int i =0; i < 4; i++) {
+            System.out.println(bil [i]);
+        }
+    }
+}
